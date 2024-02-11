741e0ccc24b91db6ddbdebfe9d7621dd8809811c
moiuha <m.bouchemella@esi-sba.dz>
2024-02-11 22:51:02 +0100

    added sing up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from "./components/Navbar"
+import { SignUp } from './SignUp'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <SignUp/>
     </div>
   )
 }
