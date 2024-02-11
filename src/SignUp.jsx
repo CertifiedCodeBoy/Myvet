eb1143cde1674e4fef97837798b7536e2a142a06
moiuha <m.bouchemella@esi-sba.dz>
2024-02-11 23:11:49 +0100

    added the gradient to the main div

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const SignUp = () => {
   return (
-    <div>
-        
+    <div className='border-2 border-black w-10/12 h-60 mx-auto my-20 rounded-3xl bg-gradient-to-bl from-indigo-900 to-white'>
+
     </div>
   )
 }
