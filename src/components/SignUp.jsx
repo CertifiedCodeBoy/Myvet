7e8974c0167314e32affe6aaa740643526c4dee0
moiuha <m.bouchemella@esi-sba.dz>
2024-02-12 11:03:47 +0100

    changes onsize

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const SignUp = () => {
   return (
-    <div className='h-screen flex '>
-      <div className='w-10/12 h-4/6 mx-auto my-20 rounded-3xl bg-gradient-to-bl' style={{ background: 'linear-gradient(-135deg, #4120A9, #FFFFFF 80%)' }}>
+    <div className='h-screen flex  '>
+      <div className='relative max-w-screen-lg w-10/12 h-5/6 mx-auto my-20 rounded-3xl bg-gradient-to-bl' style={{ background: 'linear-gradient(-135deg, #4120A9, #FFFFFF 80%)' }}>
         <div className="absolute bottom-0 left-0"></div>
       </div>
     </div>
