efe64fae49afe78584820258d83ef0b10ab579c2
moiuha <m.bouchemella@esi-sba.dz>
2024-02-12 10:31:49 +0100

    added styles

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export const SignUp = () => {
   return (
-    <div className='border-2 border-black w-10/12 h-60 mx-auto my-20 rounded-3xl bg-gradient-to-bl from-indigo-900 to-white'>
-
+    <div className='h-screen flex '>
+      <div className='w-10/12 h-4/6 mx-auto my-20 rounded-3xl bg-gradient-to-bl' style={{ background: 'linear-gradient(-135deg, #4120A9, #FFFFFF 80%)' }}>
+        <div className="absolute bottom-0 left-0"></div>
+      </div>
     </div>
   )
+
 }
