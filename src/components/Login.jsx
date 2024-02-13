b740fae54b3da5fc9a828ee55a25b37f66705cfc
CertifiedCodeBoy <aa.miloudi@esi-sba.dz>
2024-02-13 08:55:39 +0100

    Finished working on login page and added some functionalities to sign up page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,117 @@
+import { Input } from "postcss";
 import React from "react";
 
 export const Login = () => {
   return (
-    <div>
+    <div className="h-screen flex   ">
       <div
-        className="grid grid-cols-2 relative min-h-[700px] max-w-screen-xl w-10/12 h-5/6 mx-auto my-20 rounded-3xl overflow-hidden"
+        className="relative  max-w-screen-xl w-10/12 h-5/6 mx-auto my-20 rounded-3xl bg-gradient-to-bl overflow-hidden"
         style={{
-          background: "linear-gradient(-135deg, #4120A9, #FFFFFF 70%)",
+          background: "linear-gradient(-120deg, #4120A9, #FFFFFF 80%)",
+          boxShadow: "0px 2px 10outline-nonepx 2px rgba(0, 0, 0, 0.7)",
         }}
       >
-        <div className="grid-rows-2">
-          <div className="w-full h-1/3 z-50 p-10 absolute mt-10">
-            <h1 className="text-6xl font-bold">Welcome back!</h1>
-            <h2 className="m-4 text-3xl font-bold">
-              Continue your shopping adventure!
-            </h2>
+        {/* {Text} */}
+        <div className=" mx-10 absolute top-20 " style={{ color: "#4120A9" }}>
+          <h1
+            className="font-semibold text-6xl my-8 "
+            style={{ textShadow: "1px 5px 5px rgba(0, 0, 0, 0.4)" }}
+          >
+            Welcome Back !{" "}
+          </h1>
+          <h2 className="m-4 text-3xl font-bold">
+            {" "}
+            Continue your shopping advanture !
+          </h2>
+        </div>
+
+        <form style={{ fontFamily: "poppins" }}>
+          <div className="w-80 p-5 h-full absolute top-24 right-24 m-2">
+            <h1 className="text-6xl text-center m-10 font-bold">Login</h1>
+            <input
+              onFocus={(e) => {
+                e.target.style.boxShadow = "2px 2px 8px 0px rgba(0, 0, 0, 0.6)";
+              }}
+              onBlur={(e) => {
+                e.target.style.boxShadow = "none";
+              }}
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none"
+              placeholder="Email"
+              required
+            />
+
+            <input
+              onFocus={(e) => {
+                e.target.style.boxShadow = "2px 2px 8px 0px rgba(0, 0, 0, 0.6)";
+              }}
+              onBlur={(e) => {
+                e.target.style.boxShadow = "none";
+              }}
+              type="password"
+              id="password"
+              name="password"
+              className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none"
+              placeholder="Password"
+              required
+            />
+            <div className="flex items-center mr-4 ml-5">
+              <input
+                type="checkbox"
+                id="isBuyer"
+                name="isBuyer"
+                className="mr-2"
+                onChange={(e) => {
+                    const password = document.getElementById("password");
+                    if (e.target.checked) {
+                        password.type = "text";
+                    } else {
+                        password.type = "password";
+                    }
+                }}
+              />
+              <label htmlFor="isBuyer" className="text-black">
+                Show Password
+              </label>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full my-3 text-white py-3 px-4 rounded-xl"
+              style={{
+                boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)",
+                backgroundColor: "#4120A9",
+              }}
+            >
+              Log in
+            </button>
+            <h2 className="text-center my-5">OR</h2>
+            <div className="flex h-12 w-36 mx-auto">
+                <div className="w-1/2 flex align-middle justify-center">
+                    <a href="#">
+                        <img src="src/Assets/facebook.png" alt="Facebook login" className="w-16"/>
+                    </a>
+                </div>
+                <div className="w-1/2 flex align-middle justify-center">
+                    <a href="#">
+                        <img src="src/Assets/google.png" alt="Gmail login" className="w-16"/>
+                    </a>
+                </div>
+            </div>
+            <div className="my-6 text-center">
+              Don't have an account ?{" "}
+              <button style={{ color: "#4120A9" }}>Sign Up</button>
+            </div>
           </div>
-          <div className=" overflow-hidden ">
-            <img
-              src="src/Assets/man.png"
-              alt="man image"
-              className="absolute -bottom-0 left-10 h-[85%] object-contain"
-            ></img>
-          </div>
+        </form>
+        <div className=" overflow-hidden ">
+          <img
+            src="src/Assets/man.png"
+            alt="man image"
+            className="absolute -bottom-0 left-10 h-[85%] object-contain"
+          ></img>
         </div>
       </div>
     </div>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,24 +1,77 @@
-import React from 'react'
+import React from "react";
 
 export const SignUp = () => {
   return (
-    <div className='h-screen flex   '  >
-      <div className='relative  max-w-screen-lg w-10/12 h-5/6 mx-auto my-20 rounded-3xl bg-gradient-to-bl overflow-hidden'  style={{ background: 'linear-gradient(-120deg, #4120A9, #FFFFFF 80%)' ,boxShadow: '0px 0px 10px 2px rgba(0, 0, 0, 0.7)' }} >
-
+    <div className="h-screen flex   ">
+      <div
+        className="relative  max-w-screen-xl w-10/12 h-5/6 mx-auto my-20 rounded-3xl bg-gradient-to-bl overflow-hidden"
+        style={{
+          background: "linear-gradient(-120deg, #4120A9, #FFFFFF 80%)",
+          boxShadow: "0px 2px 10outline-nonepx 2px rgba(0, 0, 0, 0.7)",
+        }}
+      >
         {/* {Text} */}
-        <div className=' mx-10 absolute top-20' style={{color: '#4120A9'}}>
-          <h1 className='font-semibold text-4xl my-8 ' style={{ textShadow: '1px 10px 5px rgba(0, 0, 0, 0.4)' }} >Create new account </h1>
-          <p className='text-2xl'>Start your journey in buying and <br /> selling with ease!</p>
+        <div className=" mx-10 absolute top-20" style={{ color: "#4120A9" }}>
+          <h1
+            className="font-semibold text-6xl my-8 "
+            style={{ textShadow: "1px 5px 5px rgba(0, 0, 0, 0.4)" }}
+          >
+            Create new account{" "}
+          </h1>
+          <h2 className="m-4 text-3xl font-bold">
+            {" "}
+            Start your journey in buying and selling with ease!
+          </h2>
         </div>
 
-        <form style={{fontFamily: 'poppins'}}>
-          <div className='w-64 absolute top-36 right-24'>
-            <input type="text" id="firstName" name="firstName" className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4  " style={{ boxShadow: '2px 2px 8px 0px rgba(0, 0, 0, 0.6)' }} placeholder="First Name" />
-            <input type="text" id="lastName" name="lastName" className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4  " style={{ boxShadow: '2px 2px 8px 0px rgba(0, 0, 0, 0.6)' }} placeholder="Last Name" />
-            <input type="email" id="email" name="email" className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4  " style={{ boxShadow: '2px 2px 8px 0px rgba(0, 0, 0, 0.6)' }} placeholder="Email" />
-            <input type="password" id="password" name="password" className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4  " style={{ boxShadow: '2px 2px 8px 0px rgba(0, 0, 0, 0.6)' }} placeholder="Password" />
-            <input type="password" id="confirmPassword" name="confirmPassword" className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 " style={{ boxShadow: '2px 2px 8px 0px rgba(0, 0, 0, 0.6)' }} placeholder="Confirm Password" />
-
+        <form style={{ fontFamily: "poppins" }}>
+          <div className=" p-5 w-80 absolute right-24 m-2">
+            <h1 className="text-5xl text-center m-10 font-bold">Sign Up</h1>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none "
+              style={{ boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)" }}
+              placeholder="First Name"
+              required
+            />
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none "
+              style={{ boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)" }}
+              placeholder="Last Name"
+              required
+            />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none "
+              style={{ boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)" }}
+              placeholder="Email"
+              required
+            />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none "
+              style={{ boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)" }}
+              placeholder="Password"
+              required
+            />
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none"
+              style={{ boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)" }}
+              placeholder="Confirm Password"
+              required
+            />
 
             {/* Account type */}
             <div className=" mb-4 ">
@@ -28,30 +81,72 @@ export const SignUp = () => {
             {/* Checkboxes for buyer and seller */}
             <div className="flex justify-center mb-4">
               <div className="flex items-center mr-4">
-                <input type="checkbox" id="isBuyer" name="isBuyer" className="mr-2" />
-                <label htmlFor="isBuyer" className="text-black">Buyer</label>
+                <input
+                  type="radio"
+                  id="isBuyer"
+                  name="userType"
+                  value="buyer"
+                  className="mr-2"
+                />
+                <label htmlFor="isBuyer" className="text-black">
+                  Buyer
+                </label>
               </div>
-              <div className="flex items-center">
-                <input type="checkbox" id="isSeller" name="isSeller" className="mr-2" />
-                <label htmlFor="isSeller" className="text-black">Seller</label>
+              <div className="flex items-center mr-4">
+                <input
+                  type="radio"
+                  id="isSeller"
+                  name="userType"
+                  value="buyer"
+                  className="mr-2"
+                />
+                <label htmlFor="isSeller" className="text-black">
+                  Seller
+                </label>
               </div>
+              
             </div>
 
             {/* Checkbox for accepting privacy policy */}
-            <div className="mb-4 text-center" >
-              <input type="checkbox" id="acceptPolicy" name="acceptPolicy" className="mr-2" />
-              <label htmlFor="acceptPolicy" className="to-black">I agree with the <span className='cursor-pointer ' style={{color: '#4120A9'}}>privacy</span></label>
+            <div className="mb-4 text-center">
+              <input
+                type="checkbox"
+                id="acceptPolicy"
+                name="acceptPolicy"
+                className="mr-2 outline-none w-4 h-4"
+              />
+              <label htmlFor="acceptPolicy" className="to-black">
+                I agree with the {" "}
+                <span className="cursor-pointer " style={{ color: "#4120A9" }}>
+                  privacy
+                </span>
+              </label>
             </div>
 
-
-            <button type="submit" className="w-full my-3 text-white py-3 px-4 rounded-xl" style={{boxShadow: '2px 2px 8px 0px rgba(0, 0, 0, 0.6)' ,backgroundColor: '#4120A9'}}>Sign Up</button>
-            <div className='my-6 text-center'>Already have an account ? <button style={{color: '#4120A9'}}>Log in</button></div>
-
+            <button
+              type="submit"
+              className="w-full my-3 text-white py-3 px-4 rounded-xl"
+              style={{
+                boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)",
+                backgroundColor: "#4120A9",
+              }}
+            >
+              Sign Up
+            </button>
+            <div className="my-6 text-center">
+              Already have an account ?{" "}
+              <button style={{ color: "#4120A9" }}>Log in</button>
+            </div>
           </div>
         </form>
-        <div className=" h-4/5 w-3/5 absolute -bottom-8  left-0"><img className="w-full h-full " src='src/Assets/man.png' alt='Descripyiom'></img></div>
+        <div className=" overflow-hidden ">
+          <img
+            src="src/Assets/man.png"
+            alt="man image"
+            className="absolute -bottom-0 left-10 h-[85%] object-contain"
+          ></img>
+        </div>
       </div>
     </div>
-  )
-
-}
+  );
+};
