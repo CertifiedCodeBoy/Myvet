7ad6cc8dfac3b5a988243c3b5b1dcdf30c414904
moiuha <m.bouchemella@esi-sba.dz>
2024-02-16 23:02:06 +0100

    fixing confirm pass and error of password conf

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,151 +1,130 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Eye, EyeSlash } from "phosphor-react";
 
 const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [passwordMatchError, setPasswordMatchError] = useState(false); // State for password match error
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      setPasswordMatchError(true); // Set password match error to true
+      return; // Prevent submission if passwords don't match
+    }
+    // Reset password match error when passwords match
+    setPasswordMatchError(false);
+    // Add your signup logic here
+    console.log(formData);
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+  };
+
   return (
-    <div className="h-screen flex font-main p-5">
-      <div
-        className="relative max-w-screen-xl w-10/12 h-full mx-auto rounded-3xl bg-gradient-to-bl overflow-hidden"
-        style={{
-          background: "linear-gradient(-120deg, #4120A9, #FFFFFF 80%)",
-          boxShadow: "0px 2px 10outline-nonepx 2px rgba(0, 0, 0, 0.7)",
-        }}
-      >
-        {/* {Text} */}
-        <div className=" mx-10 absolute top-20 text-primary" id="textguyContainer">
-          <h1
-            className="font-semibold text-6xl my-8 "
-            style={{ textShadow: "1px 5px 5px rgba(0, 0, 0, 0.4)" }}
-          >
-            Create new account{" "}
-          </h1>
-          <h2 className="m-4 text-3xl font-bold">
-            {" "}
-            Start your journey in buying and selling with ease!
-          </h2>
-        </div>
-
-        <form className="">
-          <div className=" p-5 w-80 absolute right-24 m-2">
-            <h1 className="text-5xl text-center m-10 font-bold">Sign Up</h1>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none "
-              style={{ boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)" }}
-              placeholder="First Name"
-              required
-            />
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none "
-              style={{ boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)" }}
-              placeholder="Last Name"
-              required
-            />
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none "
-              style={{ boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)" }}
-              placeholder="Email"
-              required
-            />
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none "
-              style={{ boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)" }}
-              placeholder="Password"
-              required
-            />
-            <input
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none"
-              style={{ boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)" }}
-              placeholder="Confirm Password"
-              required
-            />
-
-            {/* Account type */}
-            <div className=" mb-4 ">
-              <span className="text-black">Account type:</span>
-            </div>
-
-            {/* Checkboxes for buyer and seller */}
-            <div className="flex justify-center mb-4">
-              <div className="flex items-center mr-4">
-                <input
-                  type="radio"
-                  id="isBuyer"
-                  name="userType"
-                  value="buyer"
-                  className="mr-2"
-                />
-                <label htmlFor="isBuyer" className="text-black">
-                  Buyer
-                </label>
-              </div>
-              <div className="flex items-center mr-4">
-                <input
-                  type="radio"
-                  id="isSeller"
-                  name="userType"
-                  value="buyer"
-                  className="mr-2"
-                />
-                <label htmlFor="isSeller" className="text-black">
-                  Seller
-                </label>
-              </div>
-              
-            </div>
-
-            {/* Checkbox for accepting privacy policy */}
-            <div className="mb-4 text-center">
-              <input
-                type="checkbox"
-                id="acceptPolicy"
-                name="acceptPolicy"
-                className="mr-2 outline-none w-4 h-4"
-              />
-              <label htmlFor="acceptPolicy" className="to-black">
-                I agree with the {" "}
-                <span className="cursor-pointer " style={{ color: "#4120A9" }}>
-                  privacy
-                </span>
-              </label>
-            </div>
-
-            <button
-              type="submit"
-              className="w-full my-3 text-white py-3 px-4 rounded-xl"
-              style={{
-                boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)",
-                backgroundColor: "#4120A9",
-              }}
-            >
-              Sign Up
-            </button>
-            <div className="my-6 text-center">
-              Already have an account ?{" "}
-              <Link to="/Login" className="text-[#4120A9]">Log in</Link>
+    <div className="font-main my-0 h-screen overflow-hidden flex-nowrap">
+      <div className="relative w-10/12 mx-auto">
+        <div className="h-full flex items-center pb-16 md:pb-5">
+          <div className="h-[1000px]"></div>
+          <div className="flex justify-center mx-auto w-full">
+            <div className="w-96 sm:w-[800px] p-5 shrink-1 h-1/3 border-solid shadow-2xl border border-secondary rounded-3xl bg-transparent backdrop-blur-[20px] box-shadow-custom-light">
+              <form className="flex justify-center align-middle" onSubmit={handleSubmit}>
+                <div className="sm:w-[300px]">
+                  <h1 className="text-xl sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl mb-10 font-bold drop-shadow-xl text-center text-black">
+                    Sign Up
+                  </h1>
+                  <input
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    value={formData.firstName}
+                    onChange={handleChange}
+                    className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none"
+                    placeholder="First Name"
+                    required
+                  />
+                  <input
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    value={formData.lastName}
+                    onChange={handleChange}
+                    className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none"
+                    placeholder="Last Name"
+                    required
+                  />
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none"
+                    placeholder="Email"
+                    required
+                  />
+                  <div className="relative">
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      id="password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleChange}
+                      className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none pr-10"
+                      placeholder="Password"
+                      required
+                    />
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword(!showPassword)}
+                      className="absolute inset-y-0 right-4 bottom-1/2 top-1/3 flex items-center justify-center"
+                    >
+                      {showPassword? <EyeSlash size={30}/> : <Eye size={30} />}
+                    </button>
+                  </div>
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    value={formData.confirmPassword}
+                    onChange={handleChange}
+                    className="w-full bg-gray-200 rounded-2xl py-3 px-3 mb-4 outline-none"
+                    placeholder="Confirm Password"
+                    required
+                  />
+                  {/* Styled alert for password match error */}
+                  {passwordMatchError && (
+                    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                      <strong className="font-bold">Error:</strong> Password and confirmation password do not match.
+                    </div>
+                  )}
+                  <button
+                    type="submit"
+                    className="w-full my-3 text-white py-3 px-4 rounded-xl bg-primary"
+                    style={{ boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)" }}
+                  >
+                    Sign Up
+                  </button>
+                  {/* Other UI elements */}
+                </div>
+              </form>
             </div>
           </div>
-        </form>
-        <div className=" overflow-hidden ">
-          <img
-            src="src/Assets/man.png"
-            alt="man image"
-            className="absolute -bottom-0 left-10 h-[85%] object-contain"
-          ></img>
         </div>
       </div>
     </div>
