619acdc96d2b7cf49567389d782926ccd93a312b
CertifiedCodeBoy <aa.miloudi@esi-sba.dz>
2024-02-16 02:32:18 +0100

    responsive designes added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -35,19 +35,17 @@ const Login = () => {
 
   React.useEffect(() => {
     scrollToSlow(2000, 50);
-    
   }, []);
   return (
     <div className=" font-main overflow-auto">
       <div className="relative w-10/12 mx-auto">
-        <div className="">
-          <div className=" mx-auto my-20 sm:h-8 md:h-10  pb-80 relative top-20 text-secondary "></div>
+        <div className=" h-screen flex items-end pb-16 md:pb-5 md:h-full">
           <div className="h-[1000px]"></div>
-          <div className="flex justify-center mt-80 sticky top-72 mx-auto w-full ">
-            <div className=" w-96 sm:w-[800px] p-10 shrink-1 h-full  border-solid shadow-2xl border border-secondary rounded-3xl bg-transparent backdrop-blur-[20px] box-shadow-custom-light">
+          <div className="flex justify-center mx-auto w-full ">
+            <div className=" w-96 sm:w-[800px] p-5 shrink-1 h-1/3 border-solid shadow-2xl border border-secondary rounded-3xl bg-transparent backdrop-blur-[20px] box-shadow-custom-light">
               <form className="flex justify-center align-middle">
                 <div className="  sm:w-[300px]">
-                  <h1 className=" text-2xl sm:text-xl md:text-xl lg:text-xl xl:text-4xl mb-10 font-bold drop-shadow-xl text-center text-black">
+                  <h1 className=" text-xl sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl mb-10 font-bold drop-shadow-xl text-center text-black">
                     Welcome Back !
                   </h1>
                   <h2 className="hidden text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-semibold drop-shadow-lg  text-center">
@@ -98,11 +96,12 @@ const Login = () => {
                   <button
                     type="submit"
                     className="w-full my-3 text-white py-3 px-4 rounded-xl bg-primary"
-                    onFocus={(e) => {
+                    style={{ boxShadow: "2px 2px 8px 0px rgba(0, 0, 0, 0.6)" }}
+                    onBlur={(e) => {
                       e.target.style.boxShadow =
                         "2px 2px 8px 0px rgba(0, 0, 0, 0.6)";
                     }}
-                    onBlur={(e) => {
+                    onFocus={(e) => {
                       e.target.style.boxShadow = "none";
                     }}
                   >
