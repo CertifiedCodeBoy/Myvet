233eb8f964a2b827373e642fa47e3e4e624c8291
CertifiedCodeBoy <aa.miloudi@esi-sba.dz>
2024-04-07 07:44:46 +0100

    'update'

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import UserProvider from "./contexts/UserContext.jsx";
+import UserProvider from "./contexts/UserContext";
 import SellerProvider from "./contexts/SellerContext.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
-import ProductsProvider from "./contexts/ProductsContext.jsx";
+import ProductsProvider from "./contexts/ProductsContext";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-      <ChakraProvider>
-        <ProductsProvider>
-          <UserProvider>
-            <SellerProvider>
-              <App />
-            </SellerProvider>
-          </UserProvider>
-        </ProductsProvider>
-      </ChakraProvider>
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <ProductsProvider>
+        <UserProvider>
+          <SellerProvider>
+            <App />
+          </SellerProvider>
+        </UserProvider>
+      </ProductsProvider>
+    </ChakraProvider>
+  </React.StrictMode>
 );
