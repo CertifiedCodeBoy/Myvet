41ed337d9ae09f77058498e8e43059dec9911431
CertifiedCodeBoy <aa.miloudi@esi-sba.dz>
2024-04-07 04:22:00 +0100

    'update'

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import UserProvider from "./contexts/UserContext";
+import UserProvider from "./contexts/UserContext.jsx";
 import SellerProvider from "./contexts/SellerContext.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
-import ProductsProvider from "./contexts/ProductsContext";
+import ProductsProvider from "./contexts/ProductsContext.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
